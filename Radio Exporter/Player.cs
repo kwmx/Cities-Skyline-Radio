da5e5c2fac1f7edd631b632f6a07f984f6a12065
Faisal Alhisan <b.f.x@hotmail.com>
2016-02-22 01:31:38 -0500

    First version

---
+++ Radio Exporter/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using MetroFramework.Forms;
+using Radio;
+namespace Radio_Exporter
+{
+    public partial class Player : MetroForm
+    {
+        int prevVol = 50;
+        rStream radioStream;  
+        public Player()
+        {
+            InitializeComponent();
+        }
+        private void Player_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if(txtURL.Text != null || txtURL.Text != "" || txtURL.Text != " " || txtURL.Text != string.Empty)
+            {
+                string url = txtURL.Text.Replace("\"", "");
+                Channel temp = new Channel("UNKNOWN", url);
+                radioStream = new rStream(new List<Channel>(), true, prevVol);
+                radioStream.PlayChannel(temp);
+                metroButton2.Enabled = true;
+                metroButton3.Enabled = true;
+                metroButton4.Enabled = true;
+                metroLabel1.Text = "Volume: " + radioStream.Volume;
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            prevVol = radioStream.Volume;
+            radioStream.PlayerStop();
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            radioStream.refVolume(++radioStream.Volume);
+            metroLabel1.Text = "Volume: " + radioStream.Volume;
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            radioStream.refVolume(--radioStream.Volume);
+            metroLabel1.Text = "Volume: " + radioStream.Volume;
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            if (radioStream.isMuted)
+            {
+                radioStream.Unmute();
+                metroLabel1.Text = "Volume: " + radioStream.Volume;
+                metroButton5.Text = "Mute";
+            }
+            else
+            {
+                radioStream.Mute();
+                metroLabel1.Text = "MUTED";
+                metroButton5.Text = "Unmute";
+            }
+        }
+    }
+}
---
+++ Radio Exporter/Simple.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using MetroFramework.Forms;
+using System.Windows.Forms;
+using Radio;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace Radio_Exporter
+{
+    public partial class Simple : MetroForm
+    {
+
+        public Simple()
+        {
+            InitializeComponent();
+        }
+
+        private void Simple_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+            txtTitle.Text = string.Empty;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string url = txtUrl.Text;
+            if (title == "" || title == " " || title == null || title == string.Empty)
+                title = "Unknown";
+            if (url != "" && url != " " && url != null)
+            {
+                txtList.Text += "\"" + title + "\"" + "-!-\"" + url + "\"" + Environment.NewLine;
+                if (!btnExport.Enabled)
+                    btnExport.Enabled = true;
+                txtTitle.Text = string.Empty;
+                txtUrl.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Incorrect format for URL");
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            List<Channel> channels = new List<Channel>();
+            bool writeToFile = true;
+            string[] lines = txtList.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(new string[] { "-!-" }, StringSplitOptions.None);
+                if (words.Length < 2)
+                {
+                    continue;
+                }
+                if (words.Length != 2 || !words[0].Contains("\"") || !words[1].Contains("\"") || !words[1].Contains("http"))
+                {
+                    writeToFile = false;
+                    MessageBox.Show("Error where line = '" + line + "'. Incorrect format" + " 0: " + words[0] + " 1:" + words[1] + ". Line has been ignored");
+                    continue;
+                }
+                else
+                {
+                    string title = words[0].Replace("\"", "");
+                    string url = words[1].Replace("\"", "");
+                    Channel temp = new Channel(title, url);
+                    channels.Add(temp);
+                }
+            }
+            if (writeToFile && channels.Count > 0)
+                listToXml(channels, txtName.Text);
+        }
+        //---------------------------------------------------------------------------
+        //Get list of channels and file name and then write it to file
+        //---------------------------------------------------------------------------
+        private void listToXml(List<Channel> channelsList, string name = "radio")
+        {
+            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+            {
+                Indent = true,
+                IndentChars = "\t",
+                NewLineOnAttributes = true
+            };
+            name += ".rxm";
+            using (XmlWriter writer = XmlWriter.Create(name, xmlWriterSettings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Radio");
+                foreach (Channel channel in channelsList)
+                {
+                    writer.WriteStartElement("Channel");
+                    writer.WriteAttributeString("Title", null, channel.Title);
+                    writer.WriteAttributeString("Url", null, channel.Url);
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+    }
+}
