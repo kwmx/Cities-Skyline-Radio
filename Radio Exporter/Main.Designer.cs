0b1074d8cefc49286fe4debd1d90b1a4b5d29d1e
Faisal Alhisan <b.f.x@hotmail.com>
2016-02-22 01:37:38 -0500

    First version

---
+++ Radio Exporter/Main.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Radio_Exporter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroToolTip1 = new MetroFramework.Components.MetroToolTip();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 63);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(221, 23);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Bulk exporter";
+            this.metroToolTip1.SetToolTip(this.metroButton1, "Export a bulk of radio channels (This usually is faster)");
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(23, 92);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(221, 23);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "Simple Add";
+            this.metroToolTip1.SetToolTip(this.metroButton2, "Add channels to a config file one by one in the editor");
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(23, 121);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(221, 23);
+            this.metroButton3.TabIndex = 2;
+            this.metroButton3.Text = "Test channel";
+            this.metroToolTip1.SetToolTip(this.metroButton3, "Test a channel");
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(23, 150);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(221, 23);
+            this.metroButton4.TabIndex = 3;
+            this.metroButton4.Text = "Open mod folder";
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Location = new System.Drawing.Point(23, 179);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(221, 23);
+            this.metroButton5.TabIndex = 4;
+            this.metroButton5.Text = "Download config file";
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 210);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Resizable = false;
+            this.Text = "Radio Exporter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Components.MetroToolTip metroToolTip1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+    }
+}
+
---
+++ Radio/Channel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Radio
+{
+    /// <summary>
+    /// Simple class to store the url and title of a radio stream channel
+    /// </summary>
+    public class Channel
+    {
+        private string _title;
+        private string _url;
+        /// <summary>
+        /// Class constructor takes in the title of the stream and the url
+        /// </summary>
+        public Channel(string title, string url)
+        {
+            this._title = title;
+            this._url = url;
+        }
+        public string Title { get { return _title; } }
+        public string Url { get { return _url; } }
+    }
+}
